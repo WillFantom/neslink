1713fa003ec4764704813863ece5b53189cc4e9d
Will Fantom <willf@ntom.dev>
2023-04-07 13:18:02 +0100

    refactor: Updated NsDo logic to new ns utils
    Updated NsDo to in 3 ways:
     - Support the new NsProvider system.
     - System threads used by NsDo can be flagged as dirty. This is when
       they move to a netns and fail to revert back after execution.
     - Removed the Must variants since dirty threads are now handled
       correctly.

--- do.go
+++ do.go
@@ -1,63 +1,88 @@
 package neslink
 
 import (
+	"errors"
 	"fmt"
 	"runtime"
 )
 
+var (
+	// errDirtyThread is returned when some action that moves a thread over to a
+	// netns fails to return the thread back to the netns of the caller. In the
+	// scenario where this happens, the os thread can be considered dirty and
+	// should not be reused. This error may also be wrapped into others, so
+	// errors.Is should be used to check for its presence.
+	errDirtyThread error = errors.New("system thread failed to move to expected final network namespace")
+)
+
 // NsDo executes a given function in a specified network namespace. It does so
 // in a separate OS thread in order to allow the rest of the program to continue
-// on the current network namespace. Returned is the error (if any occurred) and
-// a bool, that if false, shows that the function did not successfully return
-// the netns back to the origin, a potentially critical error.
-func NsDo(nsP NsProvider, actions ...NsAction) (error, bool) {
+// on the current network namespace. An error is returned if any netns move
+// fails or any provided action fails. Do note that if the spawned system thread
+// fails to be reverted to the network namespace of the caller, the thread is
+// considered dirty and is never unlocked (thus can not be reused).
+func NsDo(nsP NsProvider, actions ...NsAction) error {
 	// 1. get origin network namespace fd to revert back to
-	originNs, err := NPNow()()
+	originNsFd, err := NPNow().Provide().open()
 	if err != nil {
-		return fmt.Errorf("failed to reference the calling network namespace: %w", err), true
+		return fmt.Errorf("failed to open the origin netns file descriptor: %w", err)
 	}
-	defer originNs.Close()
+	defer originNsFd.close()
+
 	// 2. get new network namespace fd to switch to
-	targetNs, err := nsP()
+	targetNsFd, err := nsP.Provide().open()
 	if err != nil {
-		return fmt.Errorf("failed to reference the target network namespace: %w", err), true
+		return fmt.Errorf("failed to open the target netns file descriptor: %w", err)
 	}
-	defer targetNs.Close()
+	defer targetNsFd.close()
+
 	// 3. create error channel for new routine
 	errChan := make(chan error, 1)
 	defer close(errChan)
+
 	// 4. create new go routine
 	go func(oNs, tNs NsFd, nsActions ...NsAction) {
+
 		// 1. lock os thread for goroutine
 		runtime.LockOSThread()
-		// 2. create defer to unlock os thread
-		defer runtime.UnlockOSThread()
-		// 3. switch to new netns
-		if err := tNs.Set(); err != nil {
+
+		// 2. switch to new netns
+		if err := tNs.set(); err != nil {
 			errChan <- fmt.Errorf("failed to set netns to the target: %w", err)
 			return
 		}
-		defer oNs.Set()
-		// 4. exec ns actions
+
+		// -?- thread now dirty - perpare for cleanup
+		errSet := errors.Join(nil)
+
+		// 3. exec ns actions
 		for idx, action := range nsActions {
-			if err := action.f(); err != nil {
-				errChan <- fmt.Errorf("failed to perform ns action %d (%s): %w", idx+1, action.actionName, err)
-				return
+			if err := action.act(); err != nil {
+				errSet = errors.Join(errSet, fmt.Errorf("failed to perform ns action %d (%s)", idx+1, action.actionName), err)
+				break
 			}
 		}
-		errChan <- nil
-	}(originNs, targetNs, actions...)
-	// 5. get error from goroutine
-	err = <-errChan
-	// 6. check to make sure the final ns of the function matches the origin
-	finalNs, nsErr := NPNow()()
-	if nsErr != nil {
-		return fmt.Errorf("failed to reference the final network namespace for post-run checks: %w", nsErr), false
-	}
-	defer finalNs.Close()
-	// 7. return
-	return err, (finalNs.ID() == originNs.ID())
-	// TODO: check potential performance issue do to defer stack? maybe switch to anonymous function?
+
+		// 4. switch to origin netns
+		if err := oNs.set(); err != nil {
+			errSet = errors.Join(errSet, fmt.Errorf("failed to switch to origin ns"), err, errDirtyThread)
+		}
+
+		// 5. if thread is dirty, dont unlock thread and sleep routine forever
+		if !errors.Is(errSet, errDirtyThread) {
+			runtime.UnlockOSThread()
+			errChan <- errSet
+		} else {
+			errChan <- errSet
+			dirtyThreadSleeper := make(chan struct{})
+			<-dirtyThreadSleeper
+		}
+
+	}(originNsFd, targetNsFd, actions...)
+
+	// 5. get error from goroutine and return
+	return <-errChan
+
 }
 
 // LinkDo runs a set of link actions on a link that is obtained from the given
@@ -65,7 +90,7 @@ func NsDo(nsP NsProvider, actions ...NsAction) (error, bool) {
 // the namespace obtained by the NsProvider, thus this can be used to manage
 // links in any namespace. The actions are perfromed in the given namespace via
 // NsDo, so the returned outputs are much the same as with NsDo.
-func LinkDo(nsP NsProvider, lP LinkProvider, actions ...LinkAction) (error, bool) {
+func LinkDo(nsP NsProvider, lP LinkProvider, actions ...LinkAction) error {
 	function := func() error {
 		link, err := lP()
 		if err != nil {
@@ -79,26 +104,4 @@ func LinkDo(nsP NsProvider, lP LinkProvider, actions ...LinkAction) (error, bool
 		return nil
 	}
 	return NsDo(nsP, NAGeneric("link-action-set", function))
-}
-
-// MustNsDo performs a normal NsDo, however if it fails to revert back to the
-// origin netns, then this will trigger a panic. Any other errors are returned
-// as normal.
-func MustNsDo(nsP NsProvider, actions ...NsAction) error {
-	err, ok := NsDo(nsP, actions...)
-	if !ok {
-		panic(fmt.Errorf("nsdo failed to revert to origin ns: %w", err))
-	}
-	return err
-}
-
-// MustLinkDo performs a normal LinkDo, however if it fails to revert back to
-// the origin netns, then this will trigger a panic. Any other errors are
-// returned as normal.
-func MustLinkDo(nsP NsProvider, lP LinkProvider, actions ...LinkAction) error {
-	err, ok := LinkDo(nsP, lP, actions...)
-	if !ok {
-		panic(fmt.Errorf("linkdo failed to revert to origin ns: %w", err))
-	}
-	return err
 }
